99add80ccadafa840af52f61e04339e9d49df3e9
Fayokunmi Eboda <fayoeboda02@gmail.com>
2023-10-05 01:22:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Band name generator app, created for people who are struggling to choose a name for their startup Band
+print("Hello! Welcome to the Band name Generator.\n")
+
+city = input("What city did you grow up in?\n")
+
+
+name = input("\nWhat is the name of your pet or a pet you know?\n")
+
+Band_name = city + name
+print("\nA possible name for your band can be " + Band_name)
